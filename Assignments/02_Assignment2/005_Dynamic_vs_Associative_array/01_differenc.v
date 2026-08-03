0a067d1c56b358648b0f62abf9f958108f574418
JEBIN BENADICT J S <jebinben25@gmail.com>
2026-08-03 14:04:04 +0530

    Create 01_differenc.v
    Document the differences between dynamic and associative arrays.

---
+++ Assignments/02_Assignment2/005_Dynamic_vs_Associative_array/01_differenc.v
@@ -0,0 +1,9 @@
+/*5. How does a dynamic array differ from an associative array?
+
+
+Dynamic Array	                   Associative Array
+Size decided at runtime	         Size grows automatically when elements are added
+Uses continuous memory	           Uses memory only for existing entries
+  Index must be integer (0,1,2...)  Index can be any data type (int, string, etc.)
+Good for storing sequential data	Good for storing sparse data
+Access using position	             Access using key*/
