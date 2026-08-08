08baad8c1517e02570d9c42300325c75b299bb2d
JEBIN BENADICT J S <jebinben25@gmail.com>
2026-08-08 12:29:08 +0530

    Create 01_Predict_out.sv
    The code defines a class with a static function that attempts to access non-static members, resulting in an error.

---
+++ Assignments/03_Assignment3/005_Exercise5/01_Predict_out.sv
@@ -0,0 +1,25 @@
+class base;
+int i;
+static function get();
+int a;
+a++;
+i++;
+$display(a);
+$display(i);
+endfunction
+endclass
+base b1_h,b2_h;
+module test();
+initial
+begin
+b1_h.get();
+b1_h.get();
+b2_h.get();
+end
+endmodule
+
+/* Output of the above is 
+
+ERROR -  A static class method cannot access non static class members.
+
+*/
